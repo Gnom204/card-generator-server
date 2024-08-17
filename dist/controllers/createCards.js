8231bae8fbe815c7ba2941eda01348d865a8435e
Gnom204 <113632977+Gnom204@users.noreply.github.com>
2024-08-18 00:18:59 +0300

    featured: сервачок

--- dist/controllers/createCards.js
+++ dist/controllers/createCards.js
@@ -106,7 +106,7 @@ const getCards = (req, res) => {
         res.status(200).json({ cards });
     })
         .catch((err) => {
-        res.status(500).json({ message: "Не удалось найти карточки", err });
+        res.status(404).json({ message: "Не удалось найти карточки", err });
     });
 };
 exports.getCards = getCards;
--- src/controllers/createCards.ts
+++ src/controllers/createCards.ts
@@ -122,6 +122,6 @@ export const getCards = (req: Request, res: Response) => {
       res.status(200).json({ cards });
     })
     .catch((err) => {
-      res.status(500).json({ message: "Не удалось найти карточки", err });
+      res.status(404).json({ message: "Не удалось найти карточки", err });
     });
 };
